2a56ddc3577656aeb0672c150473ea4677c60807
steven750MC <Yasinmazahery@yahoo.com>
2025-10-03 19:59:02 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,80 @@
-// ====== ساخت کارت‌ها ======
+"use strict";
 
-// فرض کن یه لیست از کالاها داریم
-let products = [
-{ name: "او اچ ۴۷ کیوا", price: 2500000 },
-{ name: "سی اچ ۴۷ تیکون", price: 38000000 },
-{ name: "ام ای ۲۴ هیند", price: 5000000 },
-{ name: "ناو کلاس آره‌لی برک", price: 1800000000 },
-{ name: "ناو کلاس نیمیتز", price: 4500000000 },
-{ name: "ناو کلاس زوموالت", price: 3500000000 },
-{ name: "ناو کلاس تیکوندرگا", price: 1500000000 },
-{ name: "اِگیس", price: 1000000000 },
-{ name: "استرینگر", price: 38000 },
-{ name: "پارتیت", price: 3000000 },
-{ name: "نیسم", price: 1500000 },
-{ name: "موشک تاماهاک", price: 1500000 },
-{ name: "موشک جاولین", price: 175000 },
-{ name: "موشک اسکاد", price: 1000000 },
-{ name: "موشک هوا به هوا", price: 400000 },
-  { name: "ام۱ ابرامز", price: 6000000 },
-{ name: "تی ۹۰", price: 4000000 },
-{ name: "لئوپارد ۲", price: 5000000 },
-{ name: "چلنجر ۲", price: 6500000 },
-{ name: "پالایشگاه نفت", price: 5000000000 },
-{ name: "پالایشگاه گاز", price: 3000000000 },
-{ name: "معدن طلا", price: 1000000000 },
-{ name: "مزرعه گندم", price: 500000 },
-];
+const screen = document.getElementById("screen");
+const xmlns = "http://www.w3.org/2000/svg";
+const xlinkns = "http://www.w3.org/1999/xlink";
 
-// گرفتن المنت بخش نمایش کارت‌ها و بخش لیست قیمت‌ها
-let cardContainer = document.getElementById("cardContainer");
-let priceList = document.getElementById("priceList");
-let totalPriceElement = document.getElementById("totalPrice");
+window.addEventListener(
+	"pointermove",
+	(e) => {
+		pointer.x = e.clientX;
+		pointer.y = e.clientY;
+		rad = 0;
+	},
+	false
+);
 
-let total = 0;
+const resize = () => {
+	width = window.innerWidth;
+	height = window.innerHeight;
+};
 
+let width, height;
+window.addEventListener("resize", () => resize(), false);
+resize();
+
+const prepend = (use, i) => {
+	const elem = document.createElementNS(xmlns, "use");
+	elems[i].use = elem;
+	elem.setAttributeNS(xlinkns, "xlink:href", "#" + use);
+	screen.prepend(elem);
+};
+
+const N = 40;
+
+const elems = [];
+for (let i = 0; i < N; i++) elems[i] = { use: null, x: width / 2, y: 0 };
+const pointer = { x: width / 2, y: height / 2 };
+const radm = Math.min(pointer.x, pointer.y) - 20;
+let frm = Math.random();
+let rad = 0;
+
+for (let i = 1; i < N; i++) {
+	if (i === 1) prepend("Cabeza", i);
+	else if (i === 8 || i === 14) prepend("Aletas", i);
+	else prepend("Espina", i);
+}
+
+const run = () => {
+	requestAnimationFrame(run);
+	let e = elems[0];
+	const ax = (Math.cos(3 * frm) * rad * width) / height;
+	const ay = (Math.sin(4 * frm) * rad * height) / width;
+	e.x += (ax + pointer.x - e.x) / 10;
+	e.y += (ay + pointer.y - e.y) / 10;
+	for (let i = 1; i < N; i++) {
+		let e = elems[i];
+		let ep = elems[i - 1];
+		const a = Math.atan2(e.y - ep.y, e.x - ep.x);
+		e.x += (ep.x - e.x + (Math.cos(a) * (100 - i)) / 5) / 4;
+		e.y += (ep.y - e.y + (Math.sin(a) * (100 - i)) / 5) / 4;
+		const s = (162 + 4 * (1 - i)) / 50;
+		e.use.setAttributeNS(
+			null,
+			"transform",
+			`translate(${(ep.x + e.x) / 2},${(ep.y + e.y) / 2}) rotate(${(180 / Math.PI) * a
+			}) translate(${0},${0}) scale(${s},${s})`
+		);
+	}
+	if (rad < radm) rad++;
+	frm += 0.003;
+	if (rad > 60) {
+		pointer.x += (width / 2 - pointer.x) * 0.05;
+		pointer.y += (height / 2 - pointer.y) * 0.05;
+	}
+};
+
+run();
 // تابع نمایش کارت‌ها
 function showCards() {
   products.forEach((product) => {
